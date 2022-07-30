d4e53123cfefc3e2d32f1fae43961bf36bcbc8e0
nilsmanc <etereaiberia@gmail.com>
2022-07-30 16:49:54 +0300

    jwt

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { UserService } from './../../user/user.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'test',
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const data = { id: payload.sub, email: payload.email };
+    const user = await this.userService.findByCond(data);
+
+    if (!user) {
+      throw new UnauthorizedException('У вас нет доступа к этой странице');
+    }
+    return {
+      email: user.email,
+      id: user.id,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+}
