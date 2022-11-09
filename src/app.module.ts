2ae395ca2b2124706713d3fb94b03f1ff32161ae
nilsmanc <etereaiberia@gmail.com>
2022-11-09 14:16:11 +0300

    railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,19 +11,18 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { AuthModule } from './auth/auth.module';
-import { DATABASE_URL, PASSWORD } from 'variables';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      url: DATABASE_URL,
+      url: process.env.DATABASE_URL,
       type: 'postgres',
       host: 'containers-us-west-101.railway.app',
       port: 5856,
       username: 'hgqmttcurhqexj',
-      password: PASSWORD,
+      password: process.env.PASSWORD,
       database: 'railway',
       entities: [UserEntity, PostEntity, CommentEntity],
       synchronize: false,
